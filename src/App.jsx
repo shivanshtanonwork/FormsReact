cd75ac2c0b6154c40d593d241df954a6b222adb5
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-04 20:46:15 +0530

    Created Comment Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Form from './Form'
+import CommentsForm from './CommentsForm'
 
 function App() {
 
   return (
     <>
-      <Form />
+      <CommentsForm />
     </>
   )
 }
