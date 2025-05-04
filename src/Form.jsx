27e22be2c80a9f952810117d13996bbc8cf96c52
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-04 18:04:53 +0530

    Handling multiple inputs

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,66 @@
+import { useState } from "react"
+
 export default function Form() {
+    let [formData, setFormData] = useState({
+        fullName: "",
+        username: "",
+        password: ""
+    })
+
+    let handleInputChange = (event) => {
+        // let fieldName = event.target.name;
+        // let newValue = event.target.value;
+
+        setFormData((currData) => {
+            // currData[fieldName] = newValue
+            return { ...currData, [event.target.name]: event.target.value };
+        })
+    }
+
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData)
+        setFormData({
+            fullName: "",
+            username: "",
+            password: ""
+        })
+    }
+
     return (
-        <form>
-            <input placeholder="enter full name" />
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="fullName">Full Name</label> &nbsp;&nbsp;
+            <input
+                placeholder="enter full name"
+                type="text"
+                value={formData.fullName}
+                onChange={handleInputChange}
+                id="fullName"
+                name="fullName"
+            />
+            <br></br>
+            <br></br>
+            <label htmlFor="username">Username</label> &nbsp;&nbsp;
+            <input
+                placeholder="enter username"
+                type="text"
+                value={formData.username}
+                onChange={handleInputChange}
+                id="username"
+                name="username"
+            />
+            <br></br>
+            <br></br>
+            <label htmlFor="password">Password</label>
+            <input
+                placeholder="enter password"
+                type="password"
+                value={formData.password}
+                onChange={handleInputChange}
+                id="password"
+                name="password"
+            />
+            <br></br>
             <button>Submit</button>
         </form>
     )
