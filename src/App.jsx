e50ed13d3f89c8fedd27c77f6dd5fa6b201cbea6
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-05 12:35:42 +0530

    Counter basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './App.css'
 import Form from './Form'
 import CommentsForm from './CommentsForm'
 import Comment from './Comment'
+import Counter from './Counter'
 
 function App() {
 
   return (
     <>
-      <Comment />
+      <Counter />
     </>
   )
 }
